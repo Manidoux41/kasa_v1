d2463fa33505139d5ee7463ce10eb5af2d8fda4c
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-10-07 15:25:41 +0200

    Configure React app for deployment to GitHub Pages2

--- src/components/Bannner.js
+++ src/components/Bannner.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import banner from '../assets/img/banner.jpg';
 
 const Bannner = () => {
     return (
         <div className='banner'>
             <h1>Chez vous, partout et ailleurs</h1>
             <div className="overlay">
-                <img src="./assets/img/banner.jpg" alt="banner" />
+                <img src={banner} alt="banner" />
             </div>
         </div>
     );
